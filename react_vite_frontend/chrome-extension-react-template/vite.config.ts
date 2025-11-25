be202f8af5b65d4bb034083075b65dfdf877966d
David Olejniczak <david.olejniczak@icloud.com>
2025-11-25 16:54:28 -0500

    migrate chrome-extension-template into frontend

---
+++ react_vite_frontend/chrome-extension-react-template/vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite";
+import hotReloadExtension from "hot-reload-extension-vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    hotReloadExtension({
+      log: true,
+      backgroundPath: "path/to/background", // relative path to background script file
+    }),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        background: "./src/background.ts",
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === "background"
+            ? "background.js"
+            : "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
+});
